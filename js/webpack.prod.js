b644f8e46bd2f8f9441f966940f341f0a90039b8
minlare <mike@outeredgeuk.com>
2017-10-02 09:58:23 +0100

    uglifyjs

--- js/webpack.prod.js
+++ js/webpack.prod.js
@@ -1,9 +1,13 @@
 const path = require('path');
 const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     output: {
         path: path.resolve(__dirname, './../view/adminhtml/web/js')
-    }
+    },
+    plugins: [
+        new UglifyJSPlugin()
+    ]
 });
