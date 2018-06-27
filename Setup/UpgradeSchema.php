eea3be6165babc9e35f1a75f81af6f63574060de
Bruno Dias <ibroopi@gmail.com>
2018-06-27 17:33:11 +0100

    Added widget template link to layout group

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -19,6 +19,10 @@ class UpgradeSchema implements UpgradeSchemaInterface
         if (version_compare($context->getVersion(), '1.0.1', '<')) {
             $this->addUseSubcategoriesColumnToItemTable($setup);
         }
+        
+        if (version_compare($context->getVersion(), '1.0.2', '<')) {
+            $this->addLayoutGroupIdColumnToItemTable($setup);
+        }
 
         $setup->endSetup();
     }
@@ -45,6 +49,33 @@ class UpgradeSchema implements UpgradeSchemaInterface
                     'nullable' => false,
                     'default'  => 0,
                     'comment'  => 'Use Subcategory Tree'
+                ]
+            );
+        }
+    }
+    
+    /**
+     * Add the column 'use_layout_group' to the MenuItem table
+     * It allows to use layout template on a menu item
+     *
+     * @param SchemaSetupInterface $setup
+     *
+     * @return void
+     */
+    private function addLayoutGroupIdColumnToItemTable(SchemaSetupInterface $setup)
+    {
+        $connection = $setup->getConnection();
+
+        $tableItem = $setup->getTable('menu_item');
+        if ($connection->isTableExists($tableItem) == true) {
+            $connection->addColumn(
+                $tableItem,
+                'use_layout_group',
+                [
+                    'type'     => Table::TYPE_INTEGER,
+                    'nullable' => true,
+                    'default'  => null,
+                    'comment'  => 'Use Layout Group'
                 ]
             );
         }
